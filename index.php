9b6f2f8fcb416f38034b2b7cc370c0f15c2a5adf
Adri <adriendumas@live.fr>
2018-07-09 14:48:23 +0200

    ajout dans index.php du controle du paramètre: action valeur: display_home
    si le paramètre action à pour valeur: display_home, la page d'acceuil est chargée

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php 
+session_start();
+require('controller/frontend/ControllerFront.php');
+require('controller/backend/ControllerBack.php');
+
+
+try{
+    if(isset($_GET['action'])){
+        $action = $_GET['action'];
+        
+        
+//**************************************************************************//
+//********************************  FRONTEND   ********************************//
+//**************************************************************************//
+        
+        if($action == 'display_home'){
+            $ctrFront = new ControllerFront;
+            $ctrFront->display_page("home");
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+//**************************************************************************//
+//********************************  REDIRECTION   ********************************//
+//**************************************************************************//
+           
+        
+        else{
+            $ctrFront = new ControllerFront();
+            $ctrFront->display_page("home");
+        }
+    }
+    
+    else{
+        $ctrFront = new ControllerFront();
+        $ctrFront->display_page("home");
+    }
+}
+catch(Exception $e){
+    echo 'Erreur:' . $e->getMessage();
+}
