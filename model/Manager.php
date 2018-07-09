a17cf799df89c00ff4f030d5c945b0e6b869189e
Adri <adriendumas@live.fr>
2018-07-09 16:21:08 +0200

    création du fichier Manager.php dans le répertoire model
    Class Manager méthode dbConnect() >>> permet de se connecter à la base de données

---
+++ model/Manager.php
@@ -0,0 +1,8 @@
+<?php
+
+class Manager{
+    protected function dbConnect(){
+        $db = new PDO('mysql:host=localhost;dbname=blog_projet_4;charset=utf8', 'root', '');
+        return $db;
+    }
+}
