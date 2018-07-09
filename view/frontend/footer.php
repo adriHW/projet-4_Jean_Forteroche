27f0d02b0bc4cd7a63b396b36437ead49e817865
Adri <adriendumas@live.fr>
2018-07-09 17:03:23 +0200

    création du fichier footer.php dans le répertoire view/frontend
    footer.php: contenu du footer du site

---
+++ view/frontend/footer.php
@@ -0,0 +1,8 @@
+<div id="footer_container">
+    <div id="admin_container">
+        <a id='admin' style="color:white" href="index.php?action=admin_connect"><button class="btn btn_admin btn_dark_back">ADMIN</button></a>
+    </div>
+    <div id="copyright_container">
+        <p id="copy_p">COPYRIGHT  © 2018, JEAN FORTEROCHE</p>
+    </div>
+</div>
